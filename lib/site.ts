3aac9fb2b1ac0f8dae2bf7f99613c549bda878ce
tghcgu <76006000+tghcgu@users.noreply.github.com>
2026-06-06 10:56:37 +0900

    Update site meta description

--- lib/site.ts
+++ lib/site.ts
@@ -3,7 +3,7 @@ export const siteName = '日程組'
 export const siteTitle = '日程組 - 簡単日程調整・出欠管理ツール'
 
 export const siteDescription =
-  '日程組は、ログイン不要でURL共有だけで使える日程調整・出欠管理ツールです。〇△✕-とコメント、範囲指定や.ics読み込みに対応。'
+  '日程組は、ログイン不要で使える日程調整・出欠管理ツールです。URL共有、〇△✕−とコメント、候補日の一括入力、.ics読み込みに対応。'
 
 export const siteUrl = 'https://nittei-app-five.vercel.app'
 
